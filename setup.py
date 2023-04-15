8fff865f001f83715da4ba59edcdb0e6f461b600
Grevians Onula <greviansonula@gmail.com>
2023-04-15 16:13:49 +0000

    setup done

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    description="Wine Quality Package",
+    author="Grevians Onula",
+    packages=find_packages(),license="MIT"
+)
